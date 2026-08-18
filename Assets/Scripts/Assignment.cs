8212c6936dc4949707692a47606b7d2ac7947e44
RivadonS <rivadon.sung@bumail.net>
2026-08-18 21:50:26 +0700

    as08 to as11

--- Assets/Scripts/Assignment.cs
+++ Assets/Scripts/Assignment.cs
@@ -130,31 +130,95 @@ public class Assignment : MonoBehaviour
     public int as08Payment;
     public void As08_PurchasingSystemExample()
     {
-        throw new System.NotImplementedException();
+        if (as08Quantity <= 0)
+        {
+            Debug.Log("Out of stock");
+        }
+
+        if (as08Quantity > 0)
+        {
+            if (as08Payment >= as08Price)
+            {
+                Debug.Log("Purchase successful");
+                if (as08Payment > as08Price)
+                {
+                    Debug.Log($"Change: {as08Payment - as08Price} Baht");
+                }
+            }
+            else
+            {
+                Debug.Log("Insufficient funds");
+            }
+        }
     }
 
     public int as09UserChoice;
     public int as09ComputerChoice;
     public void As09_RockPaperScissorsExample()
     {
-        throw new System.NotImplementedException();
+        if (as09UserChoice < 0 || as09UserChoice > 2)
+        {
+            Debug.Log("Invalid user choice");
+            return;
+        }
+
+        if (as09UserChoice == as09ComputerChoice)
+        {
+            Debug.Log("Draw");
+        }
+        else if ((as09UserChoice == 0 && as09ComputerChoice == 2) ||
+                 (as09UserChoice == 1 && as09ComputerChoice == 0) ||
+                 (as09UserChoice == 2 && as09ComputerChoice == 1))
+        {
+            Debug.Log("User wins");
+        }
+        else
+        {
+            Debug.Log("Computer wins");
+        }
     }
 
     public string as10WeaponType;
     public int as10BaseDamage;
     public void As10_CalculateWeaponDamage()
     {
-        // TODO: Add your implementation here
-        // Example: Debug.Log("result as string");
-        throw new System.NotImplementedException();
+        switch (as10WeaponType)
+        {
+            case "Sword":
+                Debug.Log(as10BaseDamage * 1.3); break;
+            case "Axe":
+                Debug.Log(as10BaseDamage * 1.4); break;
+            case "Bow":
+                Debug.Log(as10BaseDamage * 1.2); break;
+            case "Staff":
+                Debug.Log(as10BaseDamage * 1.5); break;
+            case "Dagger":
+                Debug.Log(as10BaseDamage * 1.1); break;
+            default:
+                Debug.Log(as10BaseDamage * 1.0); break;
+        }
     }
 
     public int as11Score;
     public int as11CompletionTime;
     public void As11_DeterminePlayerRank()
     {
-        // TODO: Add your implementation here
-        // Example: Debug.Log("result as string");
-        throw new System.NotImplementedException();
+        if (as11Score < 0 || as11CompletionTime < 0)
+        {
+            Debug.Log("Invalid score or time");
+        }
+
+        string rank; int baseCoin;
+        if (as11Score >= 8000) { rank = "Gold"; baseCoin = 100; }
+        else if (as11Score >= 6000) { rank = "Silver"; baseCoin = 75; }
+        else if (as11Score >= 4000) { rank = "Bronze"; baseCoin = 50; }
+        else { rank = "participation"; baseCoin = 25; }
+
+        int timeBonus = 0;
+        if (as11CompletionTime < 30) timeBonus = 25;
+        else if (as11CompletionTime < 60) timeBonus = 10;
+
+        int totalCoin = baseCoin + timeBonus;
+        Debug.Log($"Rank: {rank}, Coins: {totalCoin}");
     }
 }
